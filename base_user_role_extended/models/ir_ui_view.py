bc9b545f59d87b67add39a1d347ef9cc2d096324
Anusha <anusha.pp@calin.co.in>
2026-06-23 10:50:35 +0200

    [IMP]Removed double Locense

--- base_user_role_extended/models/ir_ui_view.py
+++ base_user_role_extended/models/ir_ui_view.py
@@ -1,9 +1,6 @@
 # Copyright 2026 CIT Services
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-# Copyright 2026 CIT Services
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
 from odoo import models
 
 
