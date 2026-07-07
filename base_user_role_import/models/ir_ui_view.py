056e26b1c34bd47dc2208acb0c4efc00edde60f0
prayag <krishprayag@gmail.com>
2026-07-07 09:47:08 +0530

    [ADD] base_user_role_import

---
+++ base_user_role_import/models/ir_ui_view.py
@@ -0,0 +1,21 @@
+# Copyright 2026 CIT Services
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models
+
+
+class IrUiView(models.Model):
+    _inherit = "ir.ui.view"
+
+    def _get_import_manager_group_ids(self):
+        user = self.env.user
+        if user.bypass_role_policy:
+            return super()._get_import_manager_group_ids()
+
+        active_roles = user.role_line_ids.filtered(lambda r: r.is_enabled).mapped(
+            "role_id"
+        )
+        if active_roles:
+            return active_roles.mapped("group_id").ids
+
+        return super()._get_import_manager_group_ids()
